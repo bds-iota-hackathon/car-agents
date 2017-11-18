34ee56825797c42aea075c4ef13305c62998bd5a
andrewjunyoung <jun@Tianjin.local>
2017-11-18 19:14:51 +0000

    Initial website created and compiling

---
+++ web/site/server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fileSystem = require('fs');
+
+var server = http.createServer(function(req, resp){
+	fileSystem.readFile('./index.html', function(error, fileContent){
+		if(error){
+			resp.writeHead(500, {'Content-Type': 'text/plain'});
+			resp.end('Error');
+		}
+		else{
+			resp.writeHead(200, {'Content-Type': 'text/html'});
+			resp.write(fileContent);
+			resp.end();
+		}
+	});
+});
+
+server.listen(8888);
+
+console.log('Listening at: localhost:8888');
