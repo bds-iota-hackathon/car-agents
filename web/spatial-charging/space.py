07865844b3417f8fd992ceee79d292f2895bc4ba
andrewjunyoung <jun@Tianjin.local>
2017-11-18 22:06:11 +0000

    Added beginnings of file

---
+++ web/spatial-charging/space.py
@@ -0,0 +1,15 @@
+class Space:
+    dim_lens = [0,0,0]
+
+    partitions = []
+
+    get_dim_len(dim):
+        return dim_lens[dim]
+
+    get_partitions():
+        return parti
+
+
+def make_partitions(space, size):
+    for i in xrange(0, space
+    lower_left 
