94a9e238c76c728a38d8ad33a59c8087891f10b8
Nicholas Sim <nicholassimws@gmail.com>
2017-11-19 09:45:07 +0000

    charging_stn: advertise: refactor and fix

--- core/main.py
+++ core/main.py
@@ -2,7 +2,7 @@
 # File to test all stuff in the api
 
 from iotawrapper import IotaWrapper
-from charging_station import ChargingStation
+from charging_station import ChargingStation, ChargingStationStatus
 from iota import *
 import json
 import logging
@@ -19,31 +19,6 @@ logger.info(iota.connect())
 
 print iota.get_balance("U9Y9PVIBHONULDYBMY9IXBUAZEGINTZQZOTFIXYECBBPNQXBBOTQOZPCIPQLROKWJJ9UWDTUBTNQVO9RD")
 
-# logger.info("Trying to add transaction")
-# test_address = "ZRJUBCUBQKKE9RRWTIBU9VVGZBZBDQUFSNHJXMMEZVMXIDZKIOYEDA9UERLEUURJLZSMZGEWKSPTBKHMY"
-# test_message = TryteString.from_string("{data:[{node:test}]}")
-# bundle = iota.send_transfer(
-#     transfers=iota.create_transfers(test_address, test_message),
-#     inputs=[Address(address, key_index=0, security_level=0)]
-# )
-# if bundle is not None:
-#     logger.info("Bundle: {hash}".format(hash=bundle["bundle"].as_json_compatible()))
-
-# logger.info("Trying to get message by tag")
-# logger.info(iota.find_transactions([iota.get_retarded_tag()]))
-
-# 1) Advertise charging station:
-def create_station(station):
-    station.status = ChargingStation.FREE
-    tryte_msg = TryteString.from_string(json.dumps(station.get_message()))
-    bundle = iota.send_transfer(
-        transfers=iota.create_transfers(station.owner, tryte_msg, 0),
-        inputs=[Address(station.owner, key_index=0, security_level=0)]
-    )
-    if bundle is not None:
-        logger.info("Bundle: {hash}".format(hash=bundle["bundle"].as_json_compatible()))
-
-
 vendor_seed = "OJJUQWUWCW9LXFBGHIUGXQTUYYOAHJIQMJBBPOHBHCLBYDUMXLNLSUQJLNFMBITGSXGNLPFABLTQDXBM9"
 vendor_address = "OPMGOSBITOTGSZRESXAO9SGPAOOFEQ9OIPEMY9DEHPVOUULUHXIHHWBNFNMKXPEZWIMHB9JPEXSE9SFLA"
 
@@ -51,9 +26,10 @@ customer_seed = "DOUPURWDDAX9NHSVIWQN9NHMHHYFM9LFSEOJANKPHGFSHBFAKCAKDDZV9LYMCHS
 customer_address = "NSNOHLXXXPGJAUUVKZEDUPBEIJDDKA99LPOTSAD9SWKFRIVQKMQRXWMKJRNDIMANTKLZWWEVEWLQATYDB"
 
 lat = 54.409532
-lon = 18.574080
+lon = 18.2
 price = 0.5 # miota
 id = "Teststation"
 station = ChargingStation(lon, lat, price, id, vendor_address)
 
-print iota.find_transactions(18, 54, 2)
+# print iota.find_transactions(18, 54, 2)
+print station.advertise_free(iota)
