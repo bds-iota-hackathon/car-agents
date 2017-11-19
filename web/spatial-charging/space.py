c63bae78b5715fe2671a93e6344c9afde7f28b17
andrewjunyoung <jun@Tianjin.local>
2017-11-19 09:31:52 +0000

    Added tree class and partition class

--- web/spatial-charging/space.py
+++ web/spatial-charging/space.py
@@ -1,15 +1,33 @@
+class Tree(object):
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.data = None
+
 class Space:
-    dim_lens = [0,0,0]
+    def __init__(self, dim_lens=[0,0,0], partitions):
+        self.dim_lens = dim_lens
+        self.partitions = partitions
 
-    partitions = []
+    def __init__(self, dim_lens=[0,0,0]):
+        self.dim_lens = dim_lens
 
-    get_dim_len(dim):
-        return dim_lens[dim]
+    get_dim_len(self, dim):
+        return self.dim_lens[dim]
 
-    get_partitions():
-        return parti
+    make_partitions(self, size):
+        partitions = []
+        for x in range(0, get_dim_len(0)/size):
+            for y in range(0, get_dim_len(1)/size):
+                for z in range(0, get_dim_len(2)/size):
+                    pos = map((lambda dim: dim * size), [x,y,z])
+                    partitions.append(Partition(Space([size] * 3), pos))
+        return partitions
 
+class Partition():
+    def __init__(self, space, pos=[0,0,0]):
+        self.space = Space;
+        self.pos =  
 
-def make_partitions(space, size):
-    for i in xrange(0, space
-    lower_left 
+    def contains(coords):
+        if 
